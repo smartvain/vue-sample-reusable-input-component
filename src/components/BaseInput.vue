03239f72539e235fbb71aeb8ecb5cd48edab4625
Smart Vain <zlc.blue15210@gmail.com>
2022-11-05 21:09:03 +0900

    add: BaseInput component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,14 @@
+<script setup lang=ts></script>
+
+<template></template>
+
+<style>
+#app {
+  max-width: 450px;
+  margin: 60px auto;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
