c3105e09c1f74b61e5035eee9394a971cdf44b16
Smart Vain <zlc.blue15210@gmail.com>
2022-11-06 13:22:16 +0900

    add: BaseInput components to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
-<script setup lang="ts">
-</script>
-
 <template>
+  <div>
+    <BaseInput
+      v-model:modalValue="userInputName"
+      type="text"
+      label="name"
+    />
+    <BaseInput
+      v-model:modalValue="userInputEmail"
+      type="email"
+      label="email"
+    />
+    <BaseInput
+      v-model:modalValue="userInputPassword"
+      type="password"
+      label="password"
+    />
+  </div>
 </template>
+
+<script setup lang="ts">
+import BaseInput from './components/BaseInput.vue'
+import { ref } from "vue"
+
+const userInputName     = ref("")
+const userInputEmail    = ref("")
+const userInputPassword = ref("")
+</script>
 
 <style scoped>
 </style>
